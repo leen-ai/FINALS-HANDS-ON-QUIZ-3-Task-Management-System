bcaabf98d70d5a63c366fb68c4b4cd623568cc85
leen-ai <tabanaoleeann@gmail.com>
2026-05-22 22:49:41 +0800

    Update settings.py

--- backend/taskmanager/settings.py
+++ backend/taskmanager/settings.py
@@ -77,6 +77,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://finals-hands-on-quiz-3-task-managem-navy.vercel.app'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
